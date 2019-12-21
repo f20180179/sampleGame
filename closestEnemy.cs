69749a1033caeb4ee7cbf90d27fe8806c60c5450
Ashutosh Sharma <43247982+f20180179@users.noreply.github.com>
2019-12-21 22:54:26 +0530

    Add files via upload
    "enemy" to be replaced with script of your enemy objects.

---
+++ closestEnemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closestEnemy : MonoBehaviour {
+	
+	private Vector2 direction;
+	private Rigidbody2D rb;
+	[SerializeField] private float speed = 6f;
+	// Use this for initialization
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		direction = FindClosestEnemy().transform.position - this.transform.position;
+		rb.velocity = new Vector2(direction.x*speed, direction.y*speed);
+	}
+	
+	enemy FindClosestEnemy()
+	{
+		float distanceToClosestEnemy = Mathf.Infinity;
+		enemy closestEnemy = null;
+		enemy[] allEnemies = GameObject.FindObjectsOfType<enemy>();
+
+		foreach(enemy currentEnemy in allEnemies)
+		{
+			float distanceToEnemy = (currentEnemy.transform.position - this.transform.position).sqrMagnitude;
+			if(distanceToEnemy < distanceToClosestEnemy)
+			{
+				distanceToClosestEnemy = distanceToEnemy;
+				closestEnemy = currentEnemy;
+			}
+		}
+
+		return closestEnemy;
+	}
+}
